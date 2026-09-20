777d55e2f12bf8ceaddc66f9612d02ed184ebac0
Omid Azad <mail.dev.om@gmail.com>
2026-09-20 19:19:57 +0330

    FLS finished

--- frontend/src/utils/logger.js
+++ frontend/src/utils/logger.js
@@ -1,24 +1,45 @@
 const isDevelopment = __DEV__;
 
+const colors = {
+    reset: "\x1b[0m",
+
+    debug: "\x1b[38;2;167;139;250m", // #A78BFA - soft purple
+    info: "\x1b[38;2;0;87;250m",     // #0057FA - Jajisha nav blue
+    warn: "\x1b[38;2;244;180;0m",    // #F4B400 - Jajisha gold
+    error: "\x1b[38;2;248;113;113m", // #F87171 - soft coral red
+};
+
 const logger = {
     debug: (...args) => {
         if (isDevelopment) {
-            console.debug("[DEBUG]", ...args);
+            console.debug(
+                `${colors.debug}[DEBUG]${colors.reset}`,
+                ...args
+            );
         }
     },
 
     info: (...args) => {
         if (isDevelopment) {
-            console.log("[INFO]", ...args);
+            console.log(
+                `${colors.info}[INFO]${colors.reset}`,
+                ...args
+            );
         }
     },
 
     warn: (...args) => {
-        console.warn("[WARN]", ...args);
+        console.warn(
+            `${colors.warn}[WARN]${colors.reset}`,
+            ...args
+        );
     },
 
     error: (...args) => {
-        console.error("[ERROR]", ...args);
+        console.error(
+            `${colors.error}[ERROR]${colors.reset}`,
+            ...args
+        );
     },
 };
 
