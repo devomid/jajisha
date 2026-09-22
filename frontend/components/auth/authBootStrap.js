2d709e2920ff5c2e1a3c2ae0c53a9eba1a4615db
Omid Azad <mail.dev.om@gmail.com>
2026-09-22 22:06:30 +0330

    hardening [10]

--- frontend/components/auth/authBootStrap.js
+++ frontend/components/auth/authBootStrap.js
@@ -1,11 +1,16 @@
 import { useEffect } from "react";
 import { useAuth } from "../../src/hooks/useAuth";
+import logger from "../../src/utils/logger";
 
 export default function AuthBootstrap() {
     const { restoreUser } = useAuth();
 
     useEffect(() => {
-        restoreUser();
+        restoreUser().catch((error) => {
+            logger.error("Auth bootstrap failed", {
+                error: error.message,
+            });
+        });
     }, []);
 
     return null;
